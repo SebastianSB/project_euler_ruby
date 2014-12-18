b57ae791f0788527f9c6d1a3d34a8b7e6ed3b33e
Sebastian Torrente <sebastian.torrente@simplybusiness.co.uk>
2014-12-18 16:46:23 +0000

    change in problem12

--- problem3.rb
+++ problem3.rb
@@ -8,9 +8,7 @@ end
 def biggest_prime(number)
   primes_list = primes_under((number ** 0.5).to_i)
 
-  primes_list.reverse_each do |prime|
-    return prime if number  % prime == 0
-  end
+  primes_list.reverse_each {|prime| return prime if number % prime == 0}
 end
 
 puts biggest_prime(600851475143)
